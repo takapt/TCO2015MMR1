2459a3e05baa9480cba3225f83bb73fb32ed48c2
takapt <takaptpro@gmail.com>
2015-05-02 07:47:34 +0900

    modify scripts

--- compare.py
+++ compare.py
@@ -2,28 +2,35 @@
 
 import sys
 
-def get_scores(filename):
-    scores = {}
+def parse(filename):
+    results = {}
     for line in open(filename):
         s = line.split()
         seed = int(s[0])
-        score = float(s[1])
-        scores[seed] = score
-    return scores
+        np = int(s[1])
+        n = int(s[2])
+        score = float(s[3])
+        results[seed] = {'seed': seed, 'np': np, 'n': n, 'score': score}
+    return results 
 
-a, b = sys.argv[1], sys.argv[2]
-p, q = get_scores(a), get_scores(b)
+def make_line(x, y):
+    return '{:4d} {:4d} {:3d} {:>9.1f} {:>9.1f} {:>7.3f}'.format(x['seed'], x['np'], x['n'], x['score'], y['score'], x['score'] / y['score'])
+
+np_size = sys.argv[3]
+a, b = sys.argv[1] + '_' + np_size, sys.argv[2] + '_' + np_size
+p, q = parse(a), parse(b)
+
 seeds = list(set(p) & set(q))
+seeds.sort(key=lambda seed: p[seed]['score'] / q[seed]['score'])
+
 sum_ratio = 0
 for seed in seeds:
     x, y = p[seed], q[seed]
-    if y == 0:
-        continue
 
-    ratio = x / y
+    ratio = x['score'] / y['score']
     sum_ratio += ratio
 
-    print('{:>5} {:>16} {:>16} {:>7.3f}'.format(seed, x, y, ratio))
+    print(make_line(x, y))
 
 total_ratio = sum_ratio / len(seeds)
 print('total_ratio: {}'.format(total_ratio))
--- log.py
+++ log.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import os
 import shutil
@@ -8,11 +8,9 @@ import time
 
 HOME = os.environ['HOME']
 MM = os.path.join(HOME, 'mmr1')
-exe = 'a.out' if len(sys.argv) < 2 else sys.argv[1]
+exe = 'a.out' # if len(sys.argv) < 2 else sys.argv[1]
 
-result_name = None
-if len(sys.argv) >= 3:
-    result_name = sys.argv[2]
+output_filename = sys.argv[1]
 
 exe_path = os.path.join(MM, exe)
 copied_exe_path = os.path.join(MM, 'copied_' + str(random.randint(0, 10**5)))
@@ -20,8 +18,6 @@ shutil.copy(exe_path, copied_exe_path)
 
 def make_command(seed):
     command = "java -jar tester.jar -exec '{}' -novis -seed {}".format(copied_exe_path, seed)
-    if result_name:
-        command += ' -image results/{} {:06d}.png'.format(result_name, seed)
     return command
 
 def get_score(seed):
@@ -34,9 +30,14 @@ def get_score(seed):
         raise "exit"
 
     score = float(output.split()[-1])
+    np = int(output.split()[2])
+    n = int(output.split()[5])
     exe_time = time.time() - start
 
-    return {'seed': seed, 'score': score, 'time': exe_time}
+    return {'seed': seed, 'score': score, 'time': exe_time, 'np': np, 'n': n}
+
+def make_line(result):
+    return '{:4d} {:4d} {:3d} {:>9.1f}'.format(result['seed'], result['np'], result['n'], result['score'])
 
 def single(seeds):
     for seed in seeds:
@@ -44,17 +45,39 @@ def single(seeds):
         print('{:4d} {:3.3f} {:.3f}'.format(seed, result['score'], result['time']))
         sys.stdout.flush()
 
+def write_file(filename, results):
+    with open(filename, 'w') as f:
+        for result in results:
+            f.write(make_line(result) + '\n')
+
 def multi(seeds):
     from multiprocessing import Pool
     pool = Pool()
     results = pool.map(get_score, seeds)
+
+    small_results = []
+    med_results = []
+    large_results = []
     for result in results:
+        if result['np'] < 100:
+            small_results.append(result)
+        elif result['np'] < 500:
+            med_results.append(result)
+        else:
+            large_results.append(result)
+
+    output_list = small_results + med_results + large_results
+    for result in output_list:
         seed = result['seed']
-        print('{:4d} {:3.3f} {:.3f}'.format(seed, result['score'], result['time']))
+        print(make_line(result))
         sys.stdout.flush()
 
+    write_file(output_filename + '_s', small_results)
+    write_file(output_filename + '_m', med_results)
+    write_file(output_filename + '_l', large_results)
+
 try:
-#     single(range(1, 30))
+#     single(range(1, 2))
     multi(range(1, 100))
 finally:
     os.remove(copied_exe_path)
